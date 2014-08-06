e3041bdc65e73e497f3a01230d59c3df604aa9f4
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-08-06 22:12:00 +0200

    use good old 'reg query' to work around permission issues

--- scripts/appveyor-tool.ps1
+++ scripts/appveyor-tool.ps1
@@ -1,9 +1,9 @@
 Function Bootstrap {
-  Get-ChildItem -Path "HKLM:\"
-  Get-ChildItem -Path "HKLM:\SYSTEM"
-  Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet"
-  Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control"
-  Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation"
+  reg query "HKLM"
+  reg query "HKLM\SYSTEM"
+  reg query "HKLM\SYSTEM\CurrentControlSet"
+  reg query "HKLM\SYSTEM\CurrentControlSet\Control"
+  reg query "HKLM\SYSTEM\CurrentControlSet\Control\TimeZoneInformation"
   date
   $env:PATH = 'c:\Rtools\bin;c:\Rtools\MinGW\bin;c:\R\bin\i386;' + $env:PATH
   $env:PATH
