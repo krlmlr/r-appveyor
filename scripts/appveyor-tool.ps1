a958403fec3a4fbfbe2cd8ed96631ddf7a938fe6
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-08-06 20:55:47 +0200

    run tests for fakepackage

--- scripts/appveyor-tool.ps1
+++ scripts/appveyor-tool.ps1
@@ -14,3 +14,14 @@ Function Bootstrap {
   Get-Content "..\Rtools.log" -Tail 10
   date
 }
+
+Function Run_Tests {
+  date
+  $R_BUILD_ARGS = "--no-build-vignettes", "--no-manual", "--binary"
+  $R_CHECK_ARGS = "--no-build-vignettes", "--no-manual", "--as-cran"
+  Invoke-Expression 'R.exe CMD build . $R_BUILD_ARGS 2>&1 | %{ "$_" }'
+  date
+  $File = $(ls "*.tar.gz" | Sort -Property LastWriteTime -Descending | Select-Object -First 1).Name
+  Invoke-Expression 'R.exe CMD check $File $R_CHECK_ARGS 2>&1 | %{ "$_" }'
+  date
+}
