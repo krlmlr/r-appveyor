32494195f7bec098588585f7fa235c929ee67ca0
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-08-07 11:46:42 +0200

    the manual way

--- scripts/appveyor-tool.ps1
+++ scripts/appveyor-tool.ps1
@@ -8,9 +8,9 @@ Function Exec
         [Parameter(Position=1, Mandatory=0)]
         [string]$ErrorMessage = "Execution of command failed.`n$Command"
     )
-    Invoke-Expression {
-        $ErrorActionPreference = "Continue"
-        $Command
+    $ErrorActionPreference = "Continue"
+    Invoke-Expression $Command
+    $ErrorActionPreference = "Stop"
     }
     if ($LastExitCode -ne 0) {
         throw "Exec: $ErrorMessage`nExit code: $LastExitCode"
