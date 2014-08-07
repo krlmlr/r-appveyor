4c90135304a4ec7fe55f43ff1d578db2993e3c0c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-08-07 22:05:52 +0200

    really download to ..

--- scripts/appveyor-tool.ps1
+++ scripts/appveyor-tool.ps1
@@ -48,7 +48,7 @@ Function Bootstrap {
   ..\Rtools-current.exe /verysilent "/log=..\Rtools.log" | Out-Null
   Get-Content "..\Rtools.log" -Tail 10
   date
-  Invoke-WebRequest http://raw.github.com/krlmlr/r-travis/master/scripts/travis-tool.sh -OutFile "$HOME\travis-tool.sh"
+  Invoke-WebRequest http://raw.github.com/krlmlr/r-travis/master/scripts/travis-tool.sh -OutFile "..\travis-tool.sh"
 }
 
 Function Run_Tests {
