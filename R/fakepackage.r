cac4c20baeb7d1d70f2e24ee6f5ac98d670872aa
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-08-20 15:15:28 +0200

    import MASS via NAMESPACE

--- R/fakepackage.r
+++ R/fakepackage.r
@@ -1,12 +1,19 @@
+#' @docType package
+#' @import assertthat
+NULL
+
 #' Returns 3.
+#' @export
 three <- function() {
   3
 }
 
 #' Check to see if our argument is three.
+#' @export
 is_three <- function(x) {
   see_if(are_equal(3, x))
 }
 
 #' Use something from MASS
+#' @import MASS
 MASS_huber <- MASS::huber
