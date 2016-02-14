20bc25e9732ce39ad93218d336ac9d038c04f0d3
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-02-14 15:32:31 +0100

    tweak progress

--- scripts/appveyor-tool.ps1
+++ scripts/appveyor-tool.ps1
@@ -68,7 +68,7 @@ Function InstallR {
 
   $rurl = "https://cran.rstudio.com/bin/windows/base/" + $url_path + "R-" + $version + "-win.exe"
 
-  Progress ("Downloading R installer from " + $rurl)
+  Progress ("Downloading R from: " + $rurl)
   bash -c ("curl -o ../R-win.exe -L " + $rurl)
 
   Progress "Running R installer"
