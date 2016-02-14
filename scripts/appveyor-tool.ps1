e64091771e920b27757e9902fdc93f63f9bc5fe9
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-02-14 16:00:12 +0100

    also use Exec for bash

--- scripts/appveyor-tool.ps1
+++ scripts/appveyor-tool.ps1
@@ -69,7 +69,7 @@ Function InstallR {
   $rurl = "https://cran.rstudio.com/bin/windows/base/" + $url_path + "R-" + $version + "-win.exe"
 
   Progress ("Downloading R from: " + $rurl)
-  bash -c ("curl --silent -o ../R-win.exe -L " + $rurl)
+  Exec { bash -c ("curl --silent -o ../R-win.exe -L " + $rurl) }
 
   Progress "Running R installer"
   Exec { ..\R-win.exe /VERYSILENT /DIR="C:\R" }
