b935d08d2de54ff54485d6bd8b54568749bc552a
Kirill Müller <krlmlr+r@mailbox.org>
2021-11-05 08:42:22 +0100

    Up rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // hello
 RcppExport SEXP hello(SEXP a);
 RcppExport SEXP _fakepackage_hello(SEXP aSEXP) {
